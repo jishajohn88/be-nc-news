adebd4adcd3b61f0c5bbbbe89a1b2f0a7b05f724
jishajohn88 <jishajohn88@gmail.com>
2024-07-15 15:00:37 +0100

    Added the test case along with solution for task 3 GET /api

--- db/app.js
+++ db/app.js
@@ -2,7 +2,19 @@ const express = require('express')
 const app = express();
 const {getTopics} = require('../controllers/topics.controllers');
 const { customErrorHandler, serverErrorHandler } = require('../error-handlers');
+const endpoints = require('../endpoints.json')
+const fs = require('fs/promises')
 
+app.get('/api',(request,response,next)=>{
+    const path = './endpoints.json'
+    
+    fs.readFile(path,'utf-8')
+    .then((result)=>{
+        const parsedData = JSON.parse(result)
+        response.status(200).send({endpoints:parsedData})
+    })
+    
+})
 app.get('/api/topics',getTopics)
 
 app.all("*",(request,response,next) => {
