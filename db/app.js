0afdee3bea32f5964a32825770ed48f0a1c4a854
jishajohn88 <jishajohn88@gmail.com>
2024-07-15 16:51:46 +0100

    Added solution for task4 along with test cases

--- db/app.js
+++ db/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const {getTopics} = require('../controllers/topics.controllers');
+const { getArticleById } = require('../controllers/articles.controllers');
 const {  psqlErrorHandler,customErrorHandler,serverErrorHandler } = require('../error-handlers');
 const endpoints = require('../endpoints.json')
 const fs = require('fs/promises');
@@ -15,6 +16,8 @@ app.get('/api',(request,response,next)=>{
     
 })
 app.get('/api/topics',getTopics)
+
+app.get('/api/articles/:article_id',getArticleById)
 
 app.use(psqlErrorHandler)
 
