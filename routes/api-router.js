49a8cb70c4e10aef6c5dab97dddd6214e7531770
jishajohn88 <jishajohn88@gmail.com>
2024-07-18 20:02:00 +0100

    Added express routers for task 16

---
+++ routes/api-router.js
@@ -0,0 +1,21 @@
+const { getEndpoints } = require("../controllers/endpoints.controllers");
+const articlesRouter = require("./articles-router");
+const commentsRouter = require("./comments-router");
+const topicsRouter = require("./topics-router");
+const usersRouter = require("./users-router");
+
+const apiRouter = require("express").Router();
+
+apiRouter.get('/',getEndpoints)
+
+apiRouter.use('/topics',topicsRouter)
+
+apiRouter.use('/articles',articlesRouter)
+
+apiRouter.use('/:article_id/comments',articlesRouter)
+
+apiRouter.use('/:comment_id',commentsRouter)
+
+apiRouter.use('/users',usersRouter)
+
+module.exports = apiRouter
