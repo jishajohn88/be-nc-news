ee62306a7ff4c797f5208f35a8028f37f3a11b5f
jishajohn88 <jishajohn88@gmail.com>
2024-08-08 17:27:40 +0100

    Added the column comment_count

--- models/articles.models.js
+++ models/articles.models.js
@@ -4,7 +4,7 @@ const { selectTopics } = require("./topics.models.js")
 
 
 exports.selectArticles = (sort_by= 'created_at',order= 'desc',topic,p = 1,limit = 10) => {
-    const validSortBys = ['article_id', 'title', 'topic', 'author','created_at','votes','article_img_url']
+    const validSortBys = ['article_id', 'title', 'topic', 'author','created_at','votes','article_img_url','comment_count']
     const validOrders = ['asc','desc']
   
     let sqlString = `SELECT articles.author,articles.title,articles.article_id,articles.topic,articles.created_at,articles.votes,articles.article_img_url,COUNT(comments.article_id) AS comment_count FROM articles LEFT JOIN comments ON articles.article_id=comments.article_id `
